4834b93b3e0f00914536d4467ab2b2ec0ed76ffa
Rhys-Baker <bakerrhys05@gmail.com>
2025-12-06 19:00:02 +0800

    Support for newline

--- kernel.c
+++ kernel.c
@@ -82,6 +82,15 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 
 void terminal_putchar(char c) 
 {
+	// Newline
+	if(c == '\n'){
+		if (++terminal_row == VGA_HEIGHT){
+			terminal_row = 0;
+		}
+		terminal_column = 0;
+		return;
+	}
+
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
@@ -108,4 +117,5 @@ void kernel_main(void)
 
 	/* Newline support is left as an exercise. */
 	terminal_writestring("Hello, kernel World!\n");
+	terminal_writestring("This is on the next line.\n");
 }
